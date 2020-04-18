0907944157383f1ca3ebf0e40fac7f1778639dc7
RaginiPriya <rags.hp20@gmail.com>
2020-04-18 15:08:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 function addTask(task){
     // alert(task);
-    $(".taskList").append('<li class="list-group-item">'+task+'<button type="button" class="btn btn-primary btn-sm close">X</button></li>');
-    $("#taskTextbox").val("");
-    $(".close").click(function(){
-        $(this).parent().remove();
-    });
-    $("li").click(function(){
-        
-    });
+    if(task !== undefined && task !== ""){
+        $(".taskList").append('<li class="list-group-item">'+task+'<button type="button" class="btn btn-default btn-sm close">x</button></li>');
+        $("#taskTextbox").val("");
+        $(".close").click(function(){
+            $(this).parent().remove();
+        });
+        // $('input[type="checkbox"]').click(function(){
+        //     if($(this).prop("checked") == true){
+        //         $(this).parent().css("text-decoration","line-through");
+        //     }
+        //     else if($(this).prop("checked") == false){
+        //         $(this).parent().css("text-decoration","none");
+        //     }
+        // });
+        $("li").click(function(){
+            $(this).addClass("checked");
+        })
+    }
+
 }
 // $(document).ready(function(){
 //     $(".closeButton").click(function(){
